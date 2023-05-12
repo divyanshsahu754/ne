55d2b0f9bacc4623fea06dd92ea23765f065fd2f
divyanshsahu754 <divyanshsahu754@gmail.com>
2023-05-12 09:29:10 +0530

    Tic_Tac_Toe

---
+++ src/App.js
@@ -0,0 +1,15 @@
+//import logo from './logo.svg';
+import './App.css';
+import { Game } from './components/Game/game';
+
+function App() {
+  return (
+    <div className="App">
+      <Game/>
+
+      
+      </div>
+  );
+}
+
+export default App;
